5ff3e72407f60c09e04dbd4fcb8179fa10193f61
Saabbir Hossain <thesaabbir@gmail.com>
2020-03-23 08:11:51 +0600

    Create php data file

---
+++ src/views/php/data.js
@@ -0,0 +1,38 @@
+const data = {
+  builtInFunctions: [
+    {
+      name: 'implode',
+      description: ''
+    },
+    {
+      name: 'is_string',
+      description: ''
+    },
+  ],
+  builtInArrayFunctions: [
+    {
+      name: 'array_push',
+      description: "Let's you add items at the end of the array"
+    },
+    {
+      name: 'array_pop',
+      description: "Let's you remove last item from an array"
+    },
+    {
+      name: 'array_unshift',
+      description: ""
+    },
+    {
+      name: 'array_shift',
+      description: ""
+    },
+    {
+      name: 'array_values',
+      description: ""
+    },
+    {
+      name: 'unset',
+      description: ""
+    },
+  ],
+}

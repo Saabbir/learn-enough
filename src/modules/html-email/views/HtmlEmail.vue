b969331680bc594ca9332de56c06573f637f48ca
Saabbir Hossain <thesaabbir@gmail.com>
2020-04-12 17:07:10 +0600

    Add html email content

--- src/modules/html-email/views/HtmlEmail.vue
+++ src/modules/html-email/views/HtmlEmail.vue
@@ -4,14 +4,14 @@
       <!-- BACK HOME -->
       <router-link :to="{ name: 'Home' }" class="u-top-el">Back home</router-link>
 
-      <!-- Learn Enough HTML Email -->
+      <!-- HTML Email Rules -->
       <div class="c-table-responsive">
         <table class="c-table">
-          <caption>Learn Enough HTML Email</caption>
+          <caption>HTML Email Rules</caption>
           <thead>
             <tr>
-              <th>Lorem</th>
-              <th>Ipsum</th>
+              <th>Rule</th>
+              <th>Description</th>
             </tr>
           </thead>
           <tbody>
@@ -26,6 +26,57 @@
         </table>
       </div><!-- /.c-table-responsive -->
 
+      <!-- Web and Email Comparison -->
+      <div class="c-table-responsive">
+        <table class="c-table">
+          <caption>Web and Email Comparison</caption>
+          <thead>
+            <tr>
+              <th>Web</th>
+              <th>Email</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>&lt;link rel=“stylesheet”&gt;</td>
+              <td>&lt;foo style=“”&gt;</td>
+            </tr>
+            <tr>
+              <td>&lt;header&gt; &lt;section&gt; &lt;article&gt;</td>
+              <td>&lt;table&gt; &lt;tr&gt; &lt;td&gt;</td>
+            </tr>
+            <tr>
+              <td>em rem fr</td>
+              <td>px</td>
+            </tr>
+            <tr>
+              <td>background-color</td>
+              <td>bgcolor</td>
+            </tr>
+            <tr>
+              <td>margin</td>
+              <td>padding</td>
+            </tr>
+            <tr>
+              <td>rgba hsa</td>
+              <td>#000000 black</td>
+            </tr>
+            <tr>
+              <td>text-align: left;</td>
+              <td>align=“left”</td>
+            </tr>
+            <tr>
+              <td>JavaScript Interactivity</td>
+              <td>CSS Interactivity</td>
+            </tr>
+            <tr>
+              <td>Shorthand Properties</td>
+              <td>Explicit Longhand</td>
+            </tr>
+          </tbody>
+        </table>
+      </div><!-- /.c-table-responsive -->      
+
       <!-- Email Testing Clients -->
       <div class="c-table-responsive">
         <table class="c-table">
@@ -38,7 +89,7 @@
           </thead>
           <tbody>
             <tr class="info">
-              <td colspan="2"><strong><small>Web Email Client</small></strong></td>
+              <td colspan="2"><strong><small>Web-based Email Clients</small></strong></td>
             </tr>
             <tr>
               <td>AOL Mail</td>
@@ -65,10 +116,14 @@
               <td><i class="fa fa-check u-color-valid"></i></td>
             </tr>
             <tr class="info">
-              <td colspan="2"><strong><small>Desktop Email Client</small></strong></td>
+              <td colspan="2"><strong><small>Desktop Email Clients</small></strong></td>
             </tr>
             <tr>
               <td>AOL Desktop</td>
+              <td><i class="fa fa-times u-color-invalid"></i></td>
+            </tr>
+            <tr>
+              <td>Air Mail</td>
               <td><i class="fa fa-times u-color-invalid"></i></td>
             </tr>
             <tr>
@@ -77,6 +132,14 @@
             </tr>
             <tr>
               <td>IBM Notes</td>
+              <td><i class="fa fa-times u-color-invalid"></i></td>
+            </tr>
+            <tr>
+              <td>Lotus Notes</td>
+              <td><i class="fa fa-times u-color-invalid"></i></td>
+            </tr>
+            <tr>
+              <td>Entourage</td>
               <td><i class="fa fa-times u-color-invalid"></i></td>
             </tr>
             <tr>
@@ -134,6 +197,115 @@
             <tr>
               <td>Windows Live Mail</td>
               <td><i class="fa fa-times u-color-invalid"></i></td>
+            </tr>
+          </tbody>
+        </table>
+      </div><!-- /.c-table-responsive -->
+
+      <!-- Email Development Steps -->
+      <div class="c-table-responsive">
+        <table class="c-table">
+          <caption>Email Development Steps</caption>
+          <tbody>
+            <tr>
+              <td>Create basic html skeleton with doctype, head, title, meta, and body tag.</td>
+            </tr>
+            <tr>
+              <td>Create hidden preview text.</td>
+            </tr>
+            <tr>
+              <td>Structure body content using table tag for container, tr tag for rows and td tag for columns.</td>
+            </tr>
+            <tr>
+              <td>Create html structure using table tags. Use border, width, align, bgcolor, cellpadding, and cellspacing table attributes to style a table element.</td>
+            </tr>
+            <tr>
+              <td>Use inline styles, the <strong>align</strong> attribute, <strong>nested tables</strong>, and <strong>stacked tables</strong> to accomplish multiple columns in a row.</td>
+            </tr>
+            <tr>
+              <td>Apply inline css for <strong>critical content</strong> and embed css for <strong>reset</strong> and <strong>progressive enhancement functionality</strong>.</td>
+            </tr>
+            <tr>
+              <td>Apply role="presentation" to every table element to prevent screen reader software from reading every table element.</td>
+            </tr>
+            <tr>
+              <td>Use inline style attribute to style an element.</td>
+            </tr>
+            <tr>
+              <td>Use <strong>padding</strong> to create whitespace, <strong>align</strong> attribute for aligning content, and <strong>width</strong> attribute on table and td tag to for sizing.</td>
+            </tr>
+            <tr>
+              <td>The best place to apply <strong>padding</strong> is to the <strong>td</strong> element.</td>
+            </tr>
+            <tr>
+              <td>To align content apply <strong>align</strong> attribute to the table cell as well.</td>
+            </tr>
+            <tr>
+              <td>Apply <strong>align</strong> attribute to all <strong>td</strong> element avoiding inherit alignment from paren td or table tag.</td>
+            </tr>
+            <tr>
+              <td>A typical html email layout structure works like this, a root container table, a nested inner container table to constrain width to 600px, and then a third fluid table for the actual content.</td>
+            </tr>
+            <tr>
+              <td>Use percentage value for content column width.</td>
+            </tr>
+            <tr>
+              <td>Use <strong>valign</strong> attribute for aligning vertically.</td>
+            </tr>
+            <tr>
+              <td>Use <strong>47%</strong> width for each column in two columns layout and <strong>33%</strong> width for each column in three columns layout because of incorrect math rendering on Outlook.</td>
+            </tr>
+            <tr>
+              <td>Each horizontal section/line in the email content should be wrapped within it's own table element for modularity. For example every single horizontal copy line should be wrapped within it's own table, tr, and td tags. And these table element width should be 100% always.</td>
+            </tr>
+            <tr>
+              <td>Every single block level content should be wrapped within it's own table, tr, td elements. Make it a complete separate discreate component for modularity and maintenance.</td>
+            </tr>
+            <tr>
+              <td>Avoid shorthand property values and use longhand version of each property. <strong>Be explicit.</strong></td>
+            </tr>
+          </tbody>
+        </table>
+      </div><!-- /.c-table-responsive -->
+
+      <!-- Terminology -->
+      <div class="c-table-responsive">
+        <table class="c-table">
+          <caption>Terminology</caption>
+          <thead>
+            <th>Term</th>
+            <th>Description</th>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Container table</td>
+              <td>For wrapping all content of an HTML email.</td>
+            </tr>
+            <tr>
+              <td>Constrain table</td>
+              <td>For adding a max width to the email content. Using 600px constrain width is very common.</td>
+            </tr>
+            <tr>
+              <td>Content table</td>
+              <td>This table is the third nested table which contains actual content like images, headings, copy, links, buttons etc.</td>
+            </tr>
+            <tr>
+              <td>Modular component/block</td>
+              <td>Each block level content in html email should be wrapped on it's own table, tr, td element for portability and better support.</td>
+            </tr>
+          </tbody>
+        </table>
+      </div><!-- /.c-table-responsive -->
+
+      <!-- Articles related to email design, development, and marketing -->
+      <div class="c-table-responsive">
+        <table class="c-table">
+          <caption>Articles related to email design, development, and marketing</caption>
+          <tbody>
+            <tr>
+              <td>
+                <a href="http://labs.actionrocket.co/td-or-not-to-td-that-is-now-a-question" target="_blank">td or not td? That is now a question.</a>
+              </td>
             </tr>
           </tbody>
         </table>

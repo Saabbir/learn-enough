f7e5eb5549c23dca13a5a68097b4e50eb50e89ae
Saabbir Hossain <thesaabbir@gmail.com>
2021-07-25 23:29:07 +0600

    Set site title

--- vue.config.js
+++ vue.config.js
@@ -1,19 +1,24 @@
-const path = require('path')
+const path = require("path");
 
 module.exports = {
   configureWebpack: {
     // here will go any webpack settings
   },
   pluginOptions: {
-    'style-resources-loader': {
-      preProcessor: 'scss',
-      patterns: [
-        path.resolve(__dirname, './src/scss/index.scss'),
-      ]
-    }
+    "style-resources-loader": {
+      preProcessor: "scss",
+      patterns: [path.resolve(__dirname, "./src/scss/index.scss")],
+    },
   },
   css: {
     // doc: https://cli.vuejs.org/config/#css-sourcemap
-    sourceMap: true
-  }
-}
+    sourceMap: true,
+  },
+  // Set site title
+  chainWebpack: (config) => {
+    config.plugin("html").tap((args) => {
+      args[0].title = "Learn Enough";
+      return args;
+    });
+  },
+};

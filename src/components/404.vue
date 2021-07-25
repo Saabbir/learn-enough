10e791a9737fb7abe8eb5b5ce4a6d38fda919668
Saabbir Hossain <thesaabbir@gmail.com>
2021-07-25 21:45:10 +0600

    Create a 404 not found page

---
+++ src/components/404.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="l-page-wrapper">
+    <div class="u-section-padding u-fg-1">
+      <div class="l-wrap">
+        <h1>Page Not Found</h1>
+        <p>Looks like you've followed a broken link or entered a URL that doesn't exist on this site.</p>
+        <p><a href="/"><i class="fas fa-angle-left"></i> Back to home page</a></p>
+      </div><!-- /.l-wrap -->
+    </div>
+    <app-footer></app-footer>
+  </div>
+</template>
+
+<script>
+  import Footer from '@/components/Footer';
+
+  export default {
+    name: 'NotFound',
+    components: {
+      'app-footer': Footer
+    }
+  }
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,84 +1,118 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
+import Vue from "vue";
+import VueRouter from "vue-router";
 
-import JavaScriptRoutes from '@/modules/javascript/routes';
+import JavaScriptRoutes from "@/modules/javascript/routes";
+import NotFound from "../components/404.vue";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
   ...JavaScriptRoutes,
   {
-    path: '/',
-    name: 'Home',
-    component: () => import(/* webpackChunkName: "home" */ '../modules/home/views/Home.vue')
+    path: "/",
+    name: "Home",
+    component: () =>
+      import(/* webpackChunkName: "home" */ "../modules/home/views/Home.vue"),
   },
   {
-    path: '/command-line',
-    name: 'CommandLine',
+    path: "/command-line",
+    name: "CommandLine",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "command-line" */ '../modules/command-line/views/CommandLine.vue')
+    component: () =>
+      import(
+        /* webpackChunkName: "command-line" */ "../modules/command-line/views/CommandLine.vue"
+      ),
   },
   {
-    path: '/text-editor',
-    name: 'TextEditor',
-    component: () => import(/* webpackChunkName: "text-editor" */ '../modules/text-editor/views/TextEditor.vue')
+    path: "/text-editor",
+    name: "TextEditor",
+    component: () =>
+      import(
+        /* webpackChunkName: "text-editor" */ "../modules/text-editor/views/TextEditor.vue"
+      ),
   },
   {
-    path: '/git',
-    name: 'Git',
-    component: () => import(/* webpackChunkName: "git" */ '../modules/git/views/Git.vue')
+    path: "/git",
+    name: "Git",
+    component: () =>
+      import(/* webpackChunkName: "git" */ "../modules/git/views/Git.vue"),
   },
   {
-    path: '/css',
-    name: 'Css',
-    component: () => import(/* webpackChunkName: "css" */ '../modules/css/views/Css.vue')
+    path: "/css",
+    name: "Css",
+    component: () =>
+      import(/* webpackChunkName: "css" */ "../modules/css/views/Css.vue"),
   },
   {
-    path: '/javascript',
-    name: 'JavaScript',
-    component: () => import(/* webpackChunkName: "javascript" */ '../modules/javascript/views/JavaScript.vue')
+    path: "/javascript",
+    name: "JavaScript",
+    component: () =>
+      import(
+        /* webpackChunkName: "javascript" */ "../modules/javascript/views/JavaScript.vue"
+      ),
   },
   {
-    path: '/php',
-    name: 'Php',
-    component: () => import(/* webpackChunkName: "php" */ '../modules/php/views/Php.vue')
+    path: "/php",
+    name: "Php",
+    component: () =>
+      import(/* webpackChunkName: "php" */ "../modules/php/views/Php.vue"),
   },
   {
-    path: '/wordpress',
-    name: 'WordPress',
-    component: () => import(/* webpackChunkName: "wordpress" */ '../modules/wordpress/views/WordPress.vue')
+    path: "/wordpress",
+    name: "WordPress",
+    component: () =>
+      import(
+        /* webpackChunkName: "wordpress" */ "../modules/wordpress/views/WordPress.vue"
+      ),
   },
   {
-    path: '/vue',
-    name: 'Vue',
-    component: () => import(/* webpackChunkName: "wordpress" */ '../modules/vue/views/Vue.vue')
+    path: "/vue",
+    name: "Vue",
+    component: () =>
+      import(
+        /* webpackChunkName: "wordpress" */ "../modules/vue/views/Vue.vue"
+      ),
   },
   {
-    path: '/html-email',
-    name: 'HtmlEmail',
-    component: () => import(/* webpackChunkName: "html-email" */ '../modules/html-email/views/HtmlEmail.vue')
+    path: "/html-email",
+    name: "HtmlEmail",
+    component: () =>
+      import(
+        /* webpackChunkName: "html-email" */ "../modules/html-email/views/HtmlEmail.vue"
+      ),
   },
   {
-    path: '/regex',
-    name: 'Regex',
-    component: () => import(/* webpackChunkName: "regex" */ '../modules/regex/views/Regex.vue')
+    path: "/regex",
+    name: "Regex",
+    component: () =>
+      import(
+        /* webpackChunkName: "regex" */ "../modules/regex/views/Regex.vue"
+      ),
   },
   {
-    path: '/web-development',
-    name: 'WebDevelopment',
-    component: () => import(/* webpackChunkName: "web-development" */ '../modules/web-development/views/WebDevelopment.vue')
+    path: "/web-development",
+    name: "WebDevelopment",
+    component: () =>
+      import(
+        /* webpackChunkName: "web-development" */ "../modules/web-development/views/WebDevelopment.vue"
+      ),
   },
-]
+  {
+    path: "*",
+    name: "Not Found",
+    component: NotFound,
+  },
+];
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: "history",
   base: process.env.BASE_URL,
   routes,
   scrollBehavior() {
-    return { x: 0, y: 0 }
+    return { x: 0, y: 0 };
   },
-})
+});
 
-export default router
+export default router;
